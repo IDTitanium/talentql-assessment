6b70fe6c9a21f2b1d9be315cfc337c852befe811
idris lawal <idriseun222@gmail.com>
2022-08-11 13:44:05 +0100

    fix: added cors support

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const splitDate = (date) => date.split('/')
 
 app.use(limiter)
 
+app.use(function(_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.set('trust proxy', 1)
 
 app.get('/', (req, res) => {
